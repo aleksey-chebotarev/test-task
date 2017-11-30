1f8f56e183d092e62e5e08b3128c0e440a6c01e1
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-12-01 00:52:55 +0300

    Change work of parser from local to external forum

--- app/services/users_parser_service.rb
+++ app/services/users_parser_service.rb
@@ -3,12 +3,6 @@ require 'pry-rails'
 require 'net/http'
 
 class UsersParserService
-  TITLE_STRUCTURE_OF_TABLE = {
-    0 => 'username',
-    1 => 'avatar',
-    3 => 'actions'
-  }.freeze
-
   def initialize(url)
     @url = url.sub(/(\/)+$/, '') # Get url without a slash
   end
@@ -21,7 +15,7 @@ class UsersParserService
   private
 
   def check_url
-    url_with_trailing_slash = @url + '/'
+    url_with_trailing_slash = "#{@url}/"
 
     url = URI.parse(url_with_trailing_slash)
     req = Net::HTTP.new(url.host, url.port)
@@ -35,58 +29,58 @@ class UsersParserService
     agent = Mechanize.new
     agent.get(@url)
 
-    page = agent.page.link_with(text: 'Sign in').click
-
-    form = page.forms.first
-    form['user[email]']    = Rails.application.secrets.user_email
-    form['user[password]'] = Rails.application.secrets.user_password
-    form.submit.link_with(text: 'Users').click
+    agent.page.link_with(text: 'Пользователи').click
   rescue => error
     raise "Transition with home page to page of users with next error: #{error}"
   end
 
   def data_scraping
     page = transition_to_users_page
-    tbody_structure = page.parser.css('table tbody tr')
+    count_of_pages = page.css('.ipsList_inline .pagejump.clickable a').text.scan(/\d+/)[1]
 
-    data_scraping_validations(page, tbody_structure)
+    data_scraping_validations(page)
 
-    tbody_structure.each do |n|
-      username         = n.css('td:first-child').text
-      profile_url      = "#{@url}#{n.css('td:nth-child(4) a').attr('href').text}"
-      avatar_image     = n.css('td:nth-child(2) img').attr('src').text
-      avatar_image_url = "#{@url}#{avatar_image}" if avatar_image.present?
+    for i in 1..count_of_pages.to_i
+      page.css('ul.ipsMemberList > li').each do |element|
+        username     = element.css('.ipsType_subtitle a').text
+        profile_url  = element.css('.ipsType_subtitle a').attr('href').text
+        avatar_image = element.css('.ipsUserPhotoLink img').attr('src').text
 
-      # If forum_user is invalid then skip current forum_user and continue execution
-      begin
-        forum_user = ForumUser.find_or_create_by(username: username)
-        forum_user.profile_url = profile_url
-        forum_user.save!
-      rescue
-        next
+        # If forum_user is invalid then skip current forum_user and continue execution
+        begin
+          forum_user = ForumUser.find_or_create_by(username: username)
+          forum_user.profile_url = profile_url
+          forum_user.save!
+        rescue
+          next
+        end
+
+        # Update without validates, if avatar does't exist and user is correctly saved
+        forum_user.update(remote_avatar_image_url: avatar_image) if avatar_image.present?
       end
 
-      # Update without validates, if avatar does't exist and user is correctly saved
-      forum_user.update(remote_avatar_image_url: avatar_image_url) if avatar_image.present?
+      page = next_page(page)
     end
   end
 
-  def data_scraping_validations(page, tbody_structure)
-    thead_structure = page.parser.css('table thead tr')
+  def next_page(page)
+    next_element = page.css('.ipsList_inline li.page.active').first.next_element
+    next_element = next_element.at_css('a')['href']
 
-    raise 'Users page: table structure of thead is incorrect' if thead_structure.blank?
-    raise 'Users page: table structure of tbody is incorrect' if tbody_structure.blank?
+    page.link_with(href: next_element).click
+  end
 
-    thead_structure.css('th').each_with_index do |value, index|
-      title_of_table = TITLE_STRUCTURE_OF_TABLE[index]
+  def data_scraping_validations(page)
+    list_structure    = page.css('ul.ipsMemberList li')
+    current_page_link = page.css('.ipsList_inline li.page.active')
+    count_of_pages    = page.css('.ipsList_inline .pagejump.clickable a')
+    user_photo        = page.css('.ipsUserPhotoLink img')
+    block_title       = page.css('.ipsType_subtitle a')
 
-      if title_of_table.present?
-        value = value.text.downcase
-
-        unless title_of_table == value
-          raise "Position or name of #{value} are incorrect"
-        end
-      end
-    end
+    raise 'Users page: structure of list is incorrect'                   if list_structure.blank?
+    raise 'Users page: link to the current page is incorrect'            if current_page_link.blank?
+    raise 'Users page: count of pages is incorrect'                      if count_of_pages.blank?
+    raise 'Users page: structure of user photo is incorrect'             if user_photo.blank?
+    raise 'Users page: block with username or profile_url are incorrect' if block_title.blank?
   end
 end
